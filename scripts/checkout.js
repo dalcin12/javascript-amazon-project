f7ad9e4ddd80eb2043f26320942cd6fca4645eea
dalcin12 <gabrieldalcin47@gmail.com>
2024-07-11 19:45:35 -0300

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js'; // named export
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../data/cart.js'; // named export
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -32,7 +32,7 @@ cart.forEach((cartItem) => {
     }
   })
 
-  const deliveryOptionsId = cartItem.deliveryOptionsId
+  const deliveryOptionsId = cartItem.deliveryOptionId
 
   let deliveryOption;
 
@@ -101,10 +101,12 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
       ? 'FREE'
       : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionsId;
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -185,3 +187,11 @@ function saveLink(link) {
       .innerHTML = inputValue;
   }
 }
+
+document.querySelectorAll(`.js-delivery-option`)
+  .forEach((element) => {
+    element.addEventListener(`click`, () => {
+      const { productId, deliveryOptionId } = element.dataset
+      updateDeliveryOption(productId, deliveryOptionId)
+    })
+  })
