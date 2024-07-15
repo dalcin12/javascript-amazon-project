699cd84c1852d59fd03209283a5864e1c41fe444
dalcin12 <gabrieldalcin47@gmail.com>
2024-07-15 16:00:57 -0300

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -6,7 +6,6 @@ import { calculateDeliveryDate, deliveryOptions, getDeliveryOption } from '../..
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
 
-renderCheckoutHeader()
 
 export function renderOrderSummary() {
 
@@ -24,7 +23,7 @@ export function renderOrderSummary() {
     const dateString = calculateDeliveryDate(deliveryOption)
     
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
         </div>
@@ -40,7 +39,7 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}" >${cartItem.quantity}</span>
               </span>
@@ -49,7 +48,7 @@ export function renderOrderSummary() {
               </span>
               <input type="number" min="0" max="1000" class="quantity-input js-quantity-input-${matchingProduct.id}">
               <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -109,11 +108,11 @@ export function renderOrderSummary() {
 
   function deleteProduct(link) {
     const productId = link.dataset.productId
+    
     removeFromCart(productId)
-
     renderCheckoutHeader()
     renderOrderSummary()
-    renderPaymentSummary()
+    //renderPaymentSummary()
   }
 
   document.querySelectorAll(`.js-delete-link`)
