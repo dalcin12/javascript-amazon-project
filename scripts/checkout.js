eb17f1bc6e778652d39789c08cf7f37a55c93ea6
dalcin12 <gabrieldalcin47@gmail.com>
2024-07-21 19:15:24 -0300

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,12 +2,56 @@ import { renderOrderSummary } from './checkout/orderSummary.js'
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/car.js'
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
 
-loadProducts(() => { // anonymous function
-  renderOrderSummary();
-  renderPaymentSummary();
-  renderCheckoutHeader();
+Promise.all([ // array of promises that wait all the promises to finish to continue the code
+  new Promise((resolve) => { // it runs the inner function immediately
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    })
+  })
+]).then((values) => {
+  console.log(values)
+  renderCheckoutHeader()
+  renderOrderSummary()
+  renderPaymentSummary()
 })
+
+/*
+new Promise((resolve) => { // it runs the inner function immediately
+  loadProducts(() => {
+    resolve('value1');
+  });
+}).then((value) => {
+  console.log(value)
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    })
+  })
+}).then(() => {
+  renderCheckoutHeader()
+  renderOrderSummary()
+  renderPaymentSummary()
+})
+*/
+
+// Promises keep our code flat
+
+/*
+loadProducts(() => { // anonymous function
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  }) 
+})
+*/
