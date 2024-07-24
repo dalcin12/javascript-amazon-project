9827d746d344738dc1eb8bdb19c9e88422eb80a9
dalcin12 <gabrieldalcin47@gmail.com>
2024-07-23 21:31:59 -0300

    Make tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,7 +1,7 @@
 import { getProduct, loadProductsFetch } from "../data/products.js"
 import { orders } from "../data/orders.js"
+import { cart } from "../data/cart-class.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
-console.log(orders)
 
 function getOrder(orderId) {
   let matchingOrder;
@@ -18,9 +18,8 @@ function getOrder(orderId) {
 function getOrderInfo(order, productId) {
   let orderInfo = {}
   let matchingProduct;
+
   order.products.forEach(product => {
-    console.log(product)
-    
     if (product.productId === productId) {
       matchingProduct = product
     }
@@ -33,9 +32,56 @@ function getOrderInfo(order, productId) {
   return orderInfo
 }
 
+function calculateProgressPercent(order, orderInfo) {
+  let progressPercent;
+
+  progressPercent = (dayjs() - dayjs(order.orderTime)) / (dayjs(orderInfo.estimatedDeliveryTime) - dayjs(dayjs(order.orderTime))) * 100
+
+  return progressPercent
+}
+
+function renderTrackingHeader() {
+  let trackingHeader = ''
+
+  trackingHeader += `
+    <div class="amazon-header-left-section">
+      <a href="amazon.html" class="header-link">
+        <img class="amazon-logo"
+          src="images/amazon-logo-white.png">
+        <img class="amazon-mobile-logo"
+          src="images/amazon-mobile-logo-white.png">
+      </a>
+    </div>
+
+    <div class="amazon-header-middle-section">
+      <input class="search-bar" type="text" placeholder="Search">
+
+      <button class="search-button">
+        <img class="search-icon" src="images/icons/search-icon.png">
+      </button>
+    </div>
+
+    <div class="amazon-header-right-section">
+      <a class="orders-link header-link" href="orders.html">
+        <span class="returns-text">Returns</span>
+        <span class="orders-text">& Orders</span>
+      </a>
+
+      <a class="cart-link header-link" href="checkout.html">
+        <img class="cart-icon" src="images/icons/cart-icon.png">
+        <div class="cart-quantity">${cart.calculateCartQuantity()}</div>
+        <div class="cart-text">Cart</div>
+      </a>
+    </div>
+  `
+
+  document.querySelector(`.js-amazon-header`)
+    .innerHTML = trackingHeader
+}
+
 async function loadPage() {
   await loadProductsFetch()
-
+  
   const url = new URL(window.location.href)
   
   const orderId = url.searchParams.get(`orderId`)
@@ -44,6 +90,7 @@ async function loadPage() {
   const order = getOrder(orderId)
   const product = getProduct(productId)
   const orderInfo = getOrderInfo(order, productId)
+  const progressPercent = calculateProgressPercent(order, orderInfo)
 
   let html = ''
 
@@ -68,25 +115,30 @@ async function loadPage() {
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${progressPercent < 50? 'current-status' : ''}">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${progressPercent >= 50 && progressPercent < 100? 'current-status' : ''}">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${progressPercent === 100? 'current-status' : ''}">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar js-progress-bar"></div>
         </div>
       </div>
   `
 
   document.querySelector(`.main`)
     .innerHTML = html
+
+  document.querySelector(`.js-progress-bar`)
+    .setAttribute('style', `width: ${progressPercent}%`)
+
+  renderTrackingHeader()
 } 
 
 loadPage()
